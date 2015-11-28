f3136a44cd30091a4a30e6820a72f0c857359c8c
Callum Drain <callumdrain@me.com>
2015-11-28 14:40:22 +0000

    Basic idea written.

--- PASSWORD.pas
+++ PASSWORD.pas
@@ -2,15 +2,27 @@ program password;
 
 uses crt;
 
-var key : array of char;
+var key:char;
+phrase:string
 current:integer;
+done:boolean;
 
 begin
 
-    clrscr;
-    TextColor(White);
-    writeln('Welcome to a password strength checker.');
-    writeln('Please enter a password below...');
-    writeln;
-    repeat
-        key[0]
+clrscr;
+TextColor(White);
+writeln('Welcome to a password strength checker.');
+writeln('Please enter a password below...');
+writeln;
+current := 0;
+
+{repeat
+key := ReadKey;
+if key in [33..126] then
+begin
+phrase := phrase + chr(key)
+
+
+until done = true;}
+
+{Check what ReadKey returns}
